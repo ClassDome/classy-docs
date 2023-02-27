41c13109bebcf313a0d7d8bca274dd789c1e25a6
FOREVEREALIZE <felixcrat@gmail.com>
2023-02-27 09:52:01 +0000

    Update Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Classy Docs',
+  tagline: 'Dinosaurs are cool (fr tho)',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -41,8 +41,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/ClassDome/classy-docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -57,9 +57,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Classy Docs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Classy Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -110,12 +110,12 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/ClassDome/classy-docs',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ClassDome Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
